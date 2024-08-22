dc495e700d56a227d601815f259c8ddb18b0333d
--unset <--unset>
2024-08-21 21:56:10 -0400

    ready to blog

--- src/pages/blogs/exponential-derivative.js
+++ src/pages/blogs/exponential-derivative.js
@@ -4,9 +4,12 @@ import BlogLayout from "../../components/BlogLayout";
 const exp_deriv_blog = () => {
     return (
         <BlogLayout>
-            <p>
-                Test
-            </p>
+            <h3>Differentiating the Exponential</h3>
+            <h4>Introduction</h4>
+            <h4>Exposition</h4>
+            <h4>Development</h4>
+            <h4>Recapitulation</h4>
+            <h4>Coda</h4>
         </BlogLayout>
     );
 };
