df2e949d2a4e33ab8ac658a909018567a02273a2
--unset <--unset>
2024-08-23 17:32:23 -0400

    first draft ready for publication

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 import * as layout from '../components/layout.module.css';
+import { fn_body, fn_signal } from '../components/components';
 
 const TestPage = () => {
     const delta = "$\\beta$";
@@ -9,6 +10,23 @@ const TestPage = () => {
             <MathJaxContext>
                 <MathJax>{"\\( \\left( \\frac{10}{4x} \\approx 2^{12}\\right) \\)"}</MathJax>
             </MathJaxContext>
+            <p>footnote signal
+            <div>
+            <sup>
+                <a href={"#1"}>
+                    {1}
+                </a>
+            </sup>
+            <p>
+                test
+            </p>
+        </div>
+            </p>
+            <fn_body fn_number={"1"}>
+                <p>
+                    Body
+                </p>
+            </fn_body>
         </div>
     );
 };
